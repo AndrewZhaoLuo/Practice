2e8db7f665156942f6565846fb4778d45c3a452a
AndrewZhaoLuo <a>
2015-11-25 00:11:36 -0800

    Problemo 25

--- Problem25.py
+++ Problem25.py
@@ -22,10 +22,20 @@ The 12th term, F12, is the first term to contain three digits.
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 '''
 
+import math
+
 #1000 digits will come from 999 digits come from 998 digits down
+Target = 10L ** 999L #999 zeros + 1 one = 1000 digits
+index = 2
 
+F1 = 1L
+F2 = 1L
 
-F1 = 1
-F2 = 1
+while F2 < Target:
+    temp = F1 + F2
+    F1 = F2
+    F2 = temp
+    index += 1
 
+print index
 
